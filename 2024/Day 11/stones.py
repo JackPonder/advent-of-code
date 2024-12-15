47553fb8b65e73c8b5c4410ba3d1d0b9f4e91e75
Jack Ponder <jackponder3@gmail.com>
2024-12-15 01:20:24 -0500

    optimized Day 11 solution

--- 2024/Day 11/stones.py
+++ 2024/Day 11/stones.py
@@ -10,37 +10,54 @@ def main() -> None:
 
     with open(infile) as file:
         # Read file data
-        stones = [int(stone) for stone in file.read().split()]
+        stones: dict[int, int] = {}
+        for num in file.read().split():
+            num = int(num)
+            if num not in stones:
+                stones[num] = 0
+            stones[num] += 1
 
         # Update stones every blink
         for _ in range(blinks):
-            stones = getNewStones(stones)
+            stones = getNextStones(stones)
+
+        # Count number of stones after blinking
+        numStones = sum(i for i in stones.values())
 
         # Display results
-        print(f"{len(stones)} Stones")
+        print(f"{numStones} Stones")
 
 
-def getNewStones(stones: list[int]) -> list[int]:
-    """Returns new stones after blinking once"""
+def getNextStones(stones: dict[int, int]) -> dict[int, int]:
+    """Returns next set of stones after one blink"""
 
-    # Iterate over every stone and update them according to rules
-    new: list[int] = []
-    for stone in stones:
+    # Iterate over each unique stone and update them according to blinking rules
+    nextStones: dict[int, int] = {}
+    for num, occurrences in stones.items():
+        # Keep track of new stones created
+        replacements: list[int] = []
+
         # Stones engraved with 0 are replaced with 1
-        if stone == 0:
-            new.append(1)
+        if num == 0:
+            replacements.append(1)
 
         # Stones with even number of digits are split in half
-        elif len(str(stone)) % 2 == 0:
-            mid = len(str(stone)) // 2
-            new.append(int(str(stone)[:mid]))
-            new.append(int(str(stone)[mid:]))
+        elif len(str(num)) % 2 == 0:
+            mid = len(str(num)) // 2
+            replacements.append(int(str(num)[:mid]))
+            replacements.append(int(str(num)[mid:]))
 
         # Other stones are multiplied by 2024
         else:
-            new.append(stone * 2024)
+            replacements.append(num * 2024)
 
-    return new
+        # Update new stones with replacements
+        for newNum in replacements:
+            if newNum not in nextStones:
+                nextStones[newNum] = 0
+            nextStones[newNum] += occurrences
+
+    return nextStones
 
 
 if __name__ == "__main__":
