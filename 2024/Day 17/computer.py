a697d3a338b06077df60103387ab90e89cb53124
Jack Ponder <jackponder3@gmail.com>
2024-12-17 18:08:31 -0500

    minor changes to Day 17 solution

--- 2024/Day 17/computer.py
+++ 2024/Day 17/computer.py
@@ -13,33 +13,41 @@ def main() -> None:
         a, b, c = [int(line[12:]) for line in lines[:3]]
         program = [int(i) for i in lines[-1][9:].split(",")]
 
-        # Execute each instruction in the program
-        pointer = 0
-        outputs: list[int] = []
-        while pointer < len(program):
-            opcode = program[pointer]
-            operand = program[pointer + 1]
-            pointer += 2
-
-            if opcode == 0:
-                a = a // (2 ** getComboOperand(operand, a, b, c))
-            elif opcode == 1:
-                b = b ^ operand
-            elif opcode == 2:
-                b = getComboOperand(operand, a, b, c) % 8
-            elif opcode == 3 and a != 0:
-                pointer = operand
-            elif opcode == 4:
-                b = b ^ c
-            elif opcode == 5:
-                outputs.append(getComboOperand(operand, a, b, c) % 8)
-            elif opcode == 6:
-                b = a // (2 ** getComboOperand(operand, a, b, c))
-            elif opcode == 7:
-                c = a // (2 ** getComboOperand(operand, a, b, c))
+        # Get program outputs
+        outputs: list[int] = getOutputs(program, a, b, c)
 
         # Print results
         print(f"Output: {','.join(str(i) for i in outputs)}")
+
+
+def getOutputs(program: list[int], a: int, b: int, c: int) -> list[int]:
+    """Returns the outputs of a given program"""
+    
+    pointer = 0
+    outputs: list[int] = []
+    while pointer < len(program):
+        opcode = program[pointer]
+        operand = program[pointer + 1]
+        pointer += 2
+
+        if opcode == 0:
+            a = a // (2 ** getComboOperand(operand, a, b, c))
+        elif opcode == 1:
+            b = b ^ operand
+        elif opcode == 2:
+            b = getComboOperand(operand, a, b, c) % 8
+        elif opcode == 3 and a != 0:
+            pointer = operand
+        elif opcode == 4:
+            b = b ^ c
+        elif opcode == 5:
+            outputs.append(getComboOperand(operand, a, b, c) % 8)
+        elif opcode == 6:
+            b = a // (2 ** getComboOperand(operand, a, b, c))
+        elif opcode == 7:
+            c = a // (2 ** getComboOperand(operand, a, b, c))
+
+    return outputs
 
 
 def getComboOperand(operand: int, a: int, b: int, c: int) -> int:
