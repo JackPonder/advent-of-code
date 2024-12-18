6d79bb4e20b2cec4e637678f12c19040031ae84d
Jack Ponder <jackponder3@gmail.com>
2024-12-17 23:34:25 -0500

    Day 5 cleanup

--- 2024/Day 5/pages.py
+++ 2024/Day 5/pages.py
@@ -10,19 +10,26 @@ def main() -> None:
         data = file.read()
 
         # Parse rules and updates
-        sep = data.index("\n\n")
-        rules = [[int(i) for i in line.split("|")] for line in data[:sep].splitlines()]
-        updates = [[int(i) for i in line.split(",")] for line in data[(sep + 2):].splitlines()]
+        first, second = data.split("\n\n")
+        rules = [[int(i) for i in line.split("|")] for line in first.splitlines()]
+        updates = [[int(i) for i in line.split(",")] for line in second.splitlines()]
 
-        # Calculate sum of middle page numbers of correctly ordered updates
-        total = sum(update[len(update) // 2] for update in updates if isOrdered(update, rules))
+        # Calculate sum of middle page numbers of the updates
+        correct, incorrect = 0, 0
+        for update in updates:
+            orderedUpdate = ordered(update, rules)
+            if orderedUpdate == update:
+                correct += orderedUpdate[len(orderedUpdate) // 2]
+            else:
+                incorrect += orderedUpdate[len(orderedUpdate) // 2]
 
         # Print results
-        print(f"Sum of Correctly-Ordered Pages: {total}")
+        print(f"Sum of Correct Pages: {correct}")
+        print(f"Sum of Incorrect Pages: {incorrect}")
 
 
-def isOrdered(update: list[int], rules: list[list[int]]) -> bool:
-    """Returns True if a given update follow all rules, otherwise returns False"""
+def ordered(update: list[int], rules: list[list[int]]) -> list[int]:
+    """Returns the correctly-ordered version of a given update"""
 
     # Iterate through each rule and check if the update violates any
     for rule in rules:
@@ -31,11 +38,15 @@ def isOrdered(update: list[int], rules: list[list[int]]) -> bool:
         if before not in update or after not in update:
             continue
 
-        # If a before page does not precede its after page, the update does is not in the correct order 
-        if update.index(before) > update.index(after):
-            return False
+        # If a rule is broken, reorder the update to fix it, then recheck if the update is ordered correctly 
+        bIndex, aIndex = update.index(before), update.index(after)
+        if bIndex > aIndex:
+            reorderd = update.copy()
+            reorderd[bIndex] = after
+            reorderd[aIndex] = before
+            return ordered(reorderd, rules)
 
-    return True
+    return update
 
 
 if __name__ == "__main__":
