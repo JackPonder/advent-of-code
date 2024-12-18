cfc8a8aae65175b6a817194a46289f9bf4f39526
Jack Ponder <jackponder3@gmail.com>
2024-12-18 10:39:44 -0500

    added Day 18 Part 1 solution

---
+++ 2024/Day 18/ram.py
@@ -0,0 +1,66 @@
+import sys
+
+
+def main() -> None:
+    # Get input file name
+    infile = sys.argv[1]
+
+    # Get number of fallen bytes
+    bytesFallen = int(sys.argv[2])
+
+    # Get map bounds
+    bound = int(sys.argv[3])
+
+    with open(infile) as file:
+        # Read file data
+        bytes: list[tuple[int, int]] = []
+        for line in file.read().splitlines():
+            x, y = line.split(",")
+            bytes.append((int(x), int(y)))
+            
+        # Set up memory space
+        map = {(x, y): "#" if (x, y) in bytes[:bytesFallen] else "." for x in range(bound + 1) for y in range(bound + 1)}
+
+        # Calculate minimum number of steps to reach exit
+        steps = getFewestSteps(map, (0, 0), (bound, bound))
+
+        # Print results
+        print(f"Steps: {steps}")
+
+
+def getFewestSteps(map: dict[tuple[int, int], str], start: tuple[int, int], end: tuple[int, int]) -> int:
+    """Returns the minimum number of steps to reach the bottom right corner of the given area"""
+
+    # Set up inital steps for each node
+    unvisited = [start]
+    steps = {pos: 0 if pos == start else sys.maxsize for pos, val in map.items() if val != "#"}
+
+    # Traverse each node, calculating the shortest path to each adjacent node until all nodes have been searched
+    while len(unvisited) != 0:
+        # Select next unvisited node
+        node = unvisited[0]
+        
+        # Search all adjacent nodes
+        for offset in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            # Calculate adjacent node coordinates
+            row, col = node
+            rowOffset, colOffset = offset
+            adjacent = row + rowOffset, col + colOffset
+
+            # Ignore adjacent nodes that are outside map boundaries
+            if adjacent not in steps:
+                continue
+
+            # Update adjacent node if a shorter path is found
+            if steps[node] + 1 < steps[adjacent]:
+                steps[adjacent] = steps[node] + 1
+                unvisited.append(adjacent)
+  
+        # Mark current node as visited
+        unvisited.remove(node)
+    
+    return steps[end]
+
+
+if __name__ == "__main__":
+    main()
