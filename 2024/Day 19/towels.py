99e536028b98c17d762618a78b4ede5f25c23a00
Jack Ponder <jackponder3@gmail.com>
2024-12-19 20:44:05 -0500

    added Day 19 Part 2 solution

--- 2024/Day 19/towels.py
+++ 2024/Day 19/towels.py
@@ -1,5 +1,7 @@
 import sys
 
+from collections import Counter
+
 
 def main() -> None:
     # Get input file name
@@ -13,23 +15,41 @@ def main() -> None:
     patterns = firstLine.split(", ")
     designs = remainingLines.splitlines()
 
-    # Calculate number of possible designs
-    numPossible = len([design for design in designs if isPossible(design, patterns)])
+    # Calculate number of possible designs and number of different arrangements
+    numPossible = 0
+    totalArrangements = 0
+    for design in designs:
+        arrangements = getNumArrangements(design, patterns)
+        if arrangements != 0:
+            numPossible += 1
+            totalArrangements += arrangements
 
     # Print results
     print(f"Possible Designs: {numPossible}")
+    print(f"Total Arrangements: {totalArrangements}")
 
 
-def isPossible(design: str, patterns: list[str], curr: str = "") -> bool:
-    """Returns True is a design is possible to create from a list of patterns, otherwise returns False"""
+def getNumArrangements(
+    design: str, 
+    patterns: list[str], 
+    cache: Counter[str] | None = None,
+) -> int:
+    """Returns the number of different arrangements of patterns that can be used to create a design"""
 
-    if curr == design:
-        return True
-    
-    if not design.startswith(curr):
-        return False
-    
-    return any(isPossible(design, patterns, curr + pattern) for pattern in patterns)
+    # Initialize cache if none is given
+    if cache is None:
+        cache = Counter({"": 1})
+
+    # Return cached result if it exists
+    if design in cache:
+        return cache[design]
+
+    # Recursively add up number of possible arrangements
+    for pattern in patterns:
+        if design.startswith(pattern):
+            cache[design] += getNumArrangements(design[len(pattern):], patterns, cache)
+
+    return cache[design]
 
 
 if __name__ == "__main__":
