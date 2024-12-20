10a9684a5d181aad8c13845990ba81bcab997525
Jack Ponder <jackponder3@gmail.com>
2024-12-20 15:33:31 -0500

    Day 1 cleanup

--- 2024/Day 1/main.py
+++ 2024/Day 1/main.py
@@ -5,29 +5,29 @@ def main() -> None:
     # Get input file name
     infile = sys.argv[1]
 
+    # Read file data
     with open(infile) as file:
-        # Read file data
         lines = file.read().splitlines()
 
-        # Populate left and right lists
-        left: list[int] = []
-        right: list[int] = []
-        for line in lines:
-            leftNum, rightNum = [int(i) for i in line.split()]
-            left.append(leftNum)
-            right.append(rightNum)
+    # Populate left and right lists
+    left: list[int] = []
+    right: list[int] = []
+    for line in lines:
+        leftNum, rightNum = line.split()
+        left.append(int(leftNum))
+        right.append(int(rightNum))
 
-        # Sort both lists to calculate distance
-        left.sort()
-        right.sort()
+    # Sort both lists to calculate distance
+    left.sort()
+    right.sort()
 
-        # Calculate distance and similarity between both lists
-        distance = sum(abs(leftNum - rightNum) for leftNum, rightNum in zip(left, right))
-        similarity = sum(num * right.count(num) for num in left)
+    # Calculate distance and similarity between both lists
+    distance = sum(abs(leftNum - rightNum) for leftNum, rightNum in zip(left, right))
+    similarity = sum(num * right.count(num) for num in left)
 
-        # Display results
-        print(f"Distance: {distance}")
-        print(f"Similarity: {similarity}")
+    # Display results
+    print(f"Distance: {distance}")
+    print(f"Similarity: {similarity}")
 
 
 if __name__ == "__main__":
