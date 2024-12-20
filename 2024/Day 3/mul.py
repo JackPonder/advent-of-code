e901aa0a98e3f3c5c600c182478601f524de9e53
Jack Ponder <jackponder3@gmail.com>
2024-12-20 17:08:16 -0500

    Day 3 cleanup

--- 2024/Day 3/mul.py
+++ 2024/Day 3/mul.py
@@ -6,21 +6,27 @@ def main() -> None:
     # Get input file name
     infile = sys.argv[1]
 
+    # Read file data
     with open(infile) as file:
-        # Read file data
-        data = file.read()
-        
-        # Find all occurances of multiplication functions
-        matches = re.findall(r"mul\([0-9]+,[0-9]+\)", data)
+        memory = file.read()
 
-        # Iterate through matches and add up result of multiplications 
-        sum = 0
-        for match in matches:
-            num1, num2 = match[4:-1].split(",")
-            sum += int(num1) * int(num2)
+    # Search through memory and execute instructions
+    s1, s2 = 0, 0
+    enabled = True
+    for i in range(len(memory)):
+        if match := re.match(r"mul\([0-9]+,[0-9]+\)", memory[i:]):
+            num1, num2 = match.group()[4:-1].split(",")
+            s1 += int(num1) * int(num2)
+            if enabled:
+                s2 += int(num1) * int(num2)
+        elif re.match(r"do\(\)", memory[i:]):
+            enabled = True
+        elif re.match(r"don't\(\)", memory[i:]):
+            enabled = False
 
-        # Display results
-        print(f"Sum: {sum}")
+    # Display results
+    print(f"Part 1: {s1}")
+    print(f"Part 2: {s2}")
 
 
 if __name__ == "__main__":
