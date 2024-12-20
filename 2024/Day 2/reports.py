e9b578d923dcb5b8e049c5d949b8e0e29f6d1e2d
Jack Ponder <jackponder3@gmail.com>
2024-12-20 16:16:54 -0500

    Day 2 cleanup

--- 2024/Day 2/reports.py
+++ 2024/Day 2/reports.py
@@ -1,53 +1,51 @@
 import sys
+import math
 
 
 def main() -> None:
     # Get input file name
     infile = sys.argv[1]
 
+    # Read file data
     with open(infile) as file:
-        # Read file data
-        reports = file.read().splitlines()
+        reports = [[int(level) for level in report.split()] for report in file.read().splitlines()]
 
-        # Iterate through reports and count the number of safe reports
-        numSafe = 0
-        for report in reports:
-            levels = [int(i) for i in report.split()]
-            if isSafe(levels):
-                numSafe += 1
+    # Iterate through reports and count how many are safe
+    safe, dampened = 0, 0
+    for report in reports:
+        if isSafe(report):
+            safe += 1
 
-        # Display results
-        print(f"Safe Reports: {numSafe}")
+        for i in range(len(report)):
+            if isSafe(report[:i] + report[(i + 1):]):
+                dampened += 1
+                break
+
+    # Display results
+    print(f"Safe Reports: {safe}")
+    print(f"Dampened Reports: {dampened}")
 
 
 def isSafe(report: list[int]) -> bool:
-    """Returns True if a report is safe, otherwise returns False"""
+    """Returns True if a report is safe and False if a report is unsafe"""
 
     # Determine if levels are increasing or decreasing
-    change = sign(report[1] - report[0])
+    slope = math.copysign(1, report[1] - report[0])
 
     # Iterate through levels checking if each level is safe
     for i, level in enumerate(report[:-1]):
+        # Calculate difference between current level and next level
+        diff = report[i + 1] - level
+
         # Report is unsafe if levels are not all increasing or decreasing 
-        if sign(report[i + 1] - level) != change:
+        if math.copysign(1, diff) != slope:
             return False
 
         # Report is unsafe if adjacent levels differ by less than 1 or more than 3 
-        if not (1 <= abs(report[i + 1] - level) <= 3):
+        if abs(diff) < 1 or abs(diff) > 3:
             return False
 
     return True
-
-
-def sign(n: int) -> int:
-    """Returns 1 is n is positive, -1 if n is negative, and 0 if n is 0"""
-
-    if n > 0:
-        return 1
-    if n < 0:
-        return -1
-
-    return 0
 
 
 if __name__ == "__main__":
