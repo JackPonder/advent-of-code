982ce41cf026ee79e7af549f8f3414caf77d70fe
Jack Ponder <jackponder3@gmail.com>
2024-12-20 23:29:22 -0500

    Day 8 cleanup

--- 2024/Day 8/antinodes.py
+++ 2024/Day 8/antinodes.py
@@ -5,25 +5,24 @@ def main() -> None:
     # Get input file name
     infile = sys.argv[1]
 
+    # Read map data
     with open(infile) as file:
-        # Read map data 
-        map: dict[tuple[int, int], str] = {}
-        antennas: list[tuple[int, int]] = []
-        for row, line in enumerate(file.read().splitlines(), 1):
-            for col, space in enumerate(line, 1):
-                map[(row, col)] = space
-                if space != ".":
-                    antennas.append((row, col))
+        map = {(row, col): val for row, line in enumerate(file.read().splitlines()) for col, val in enumerate(line)}
 
-        # Calculate the number of antinodes on the map
-        antinodes = getAntinodes(map, antennas)
+    # Calculate the number of antinodes on the map
+    antinodes = getAntinodes(map)
+    updatedAntinodes = getAntinodes(map, extended=True)
 
-        # Print results
-        print(f"Antinodes: {antinodes}")
+    # Print results
+    print(f"Antinodes: {antinodes}")
+    print(f"Updated Antinodes: {updatedAntinodes}")
 
 
-def getAntinodes(map: dict[tuple[int, int], str], antennas: list[tuple[int, int]]) -> int:
+def getAntinodes(map: dict[tuple[int, int], str], extended: bool = False) -> int:
     """Returns the number of antinodes on a map"""
+
+    # Get all antenna locations
+    antennas = [pos for pos, val in map.items() if val != "."]
 
     # Iterate through each antenna location add up the number of unique antinodes 
     antinodes: set[tuple[int, int]] = set()
@@ -37,18 +36,25 @@ def getAntinodes(map: dict[tuple[int, int], str], antennas: list[tuple[int, int]
             if map[l1] != map[l2]:
                 continue
 
-            # Calculate antinode positions 
+            # Calculate antinode locations until antinodes fall outside map boundaries
             r1, c1 = l1
             r2, c2 = l2
             dr, dc = r2 - r1, c2 - c1
-            a1 = r1 - dr, c1 - dc
-            a2 = r2 + dr, c2 + dc
+            magnitude = 0
+            while True:
+                a1 = r1 - dr * magnitude, c1 - dc * magnitude
+                a2 = r2 + dr * magnitude, c2 + dc * magnitude
 
-            # Do not include antinodes that are outside map boundaries 
-            if a1 in map:
-                antinodes.add(a1)
-            if a2 in map:
-                antinodes.add(a2)
+                if a1 not in map and a2 not in map:
+                    break
+
+                if magnitude == 1 or extended:
+                    if a1 in map:
+                        antinodes.add(a1)
+                    elif a2 in map:
+                        antinodes.add(a2)
+
+                magnitude += 1
 
     return len(antinodes)
 
